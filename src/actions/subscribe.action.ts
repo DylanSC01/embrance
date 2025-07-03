4dc58fa2c39fd8382da4a6e9213bd9948312a1d4
DilanSC <dilan.j.sibaja.cruz@accenture.com>
2025-07-03 07:00:35 -0600

    fix: email console.log added

--- src/actions/subscribe.action.ts
+++ src/actions/subscribe.action.ts
@@ -3,7 +3,7 @@ import { delay } from "@/lib/utils"
 
 export const subscribeAction = async (email:string) => {
     await delay(1000);
-    
+    console.log("subscribeAction called with email:", email);
     //throw new Error("This is a test error");
 
     return
